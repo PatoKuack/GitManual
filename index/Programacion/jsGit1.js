edc6e998a670ed828deb44d10e9c2f979cfe1922
Pato Kuack <nocturno_x_13514@hotmail.com>
2021-07-07 12:52:29 -0500

    Creación de repositorios y de llaves SSH

---
+++ index/Programacion/jsGit1.js
@@ -0,0 +1,9 @@
+const dataTitle = Array.from(document.getElementsByTagName('dt'));
+dataTitle.map(makeBolt => makeBolt.classList.add('negrita'));
+
+const dtSpanes = Array.from(document.querySelectorAll('span'));
+// dtSpanes.map( colorea => colorea.style.fontweight = ('initial'));
+dtSpanes.map( colorea => colorea.style.color = ('gray'));
+
+const imags = Array.from(document.getElementsByTagName('img'));
+imags.map(makeSize => makeSize.classList.add('tamañito'));

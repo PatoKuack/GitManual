e3d1a2166bf0ae41bd0a7e75fe49045413c601e8
Pato Kuack <nocturno_x_13514@hotmail.com>
2021-07-09 20:48:55 -0500

    creaciòn de TAGs

--- index/Programacion/jsGit1.js
+++ index/Programacion/jsGit1.js
@@ -2,7 +2,6 @@ const dataTitle = Array.from(document.getElementsByTagName('dt'));
 dataTitle.map(makeBolt => makeBolt.classList.add('negrita'));
 
 const dtSpanes = Array.from(document.querySelectorAll('span'));
-// dtSpanes.map( colorea => colorea.style.fontweight = ('initial'));
 dtSpanes.map( colorea => colorea.style.color = ('gray'));
 
 const imags = Array.from(document.getElementsByTagName('img'));
